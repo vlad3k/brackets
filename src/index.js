cfeff90ac33ee5d94d83169c3e50014741f464d6
vlad3k <vlad3k@bk.ru>
2020-03-19 01:44:27 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let i = 0;
+    while (i < bracketsConfig.length) {
+        let couple = bracketsConfig[i].join('');
+        if (str.includes(couple)) { str = str.replace(couple, ''); i = 0; }
+        else { i += 1 }
+    }
+    return str.length === 0;
+};
